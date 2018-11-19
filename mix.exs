ae033b343c44584d1a4113be2bc2c0449ca34d23
Tomohiko Himura <eiel.hal@gmail.com>
2018-11-20 00:00:32 +0900

    feat: setup env

--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule Appenv.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      env: [value: "load mix.exs"]
     ]
   end
 
